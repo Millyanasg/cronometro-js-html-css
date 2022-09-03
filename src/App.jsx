8c91faa1599ac505acc7e72a9e8f81fc811939d1
tomast1337 <vycasnicolas@gmail.com>
2022-09-02 21:19:42 -0300

    Adicionado titulo a pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Conometro from "./components/Conometro";
 import Clock from "./components/Clock";
-export default () => (
-    <>
-        <Clock />
-        <h1>
-            Millyana Souza
-        </h1>
-        <Conometro />
-    </>
-);
+export default () => {
+    React.useEffect(() => {
+        document.title = "Cronometro Millyana";
+    });
+    
+    return (
+        <>
+            <Clock />
+            <h1>
+                Millyana Souza
+            </h1>
+            <Conometro />
+        </>
+    );
+};
